f72825c233d07b36c963bab53ec972c77292853d
Ahmed yehia <97615630+Ahmedyehia12@users.noreply.github.com>
2023-12-04 19:36:16 +0200

    Merge remote-tracking branch 'origin/binary'
    # Conflicts:
    #	.idea/workspace.xml
    #	compressed.txt
    #	src/VectorQuantization.java

--- src/VectorQuantization.java
+++ src/VectorQuantization.java
@@ -1,7 +1,8 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Vector;
-
 public class VectorQuantization implements Algorithm {
     ArrayList<ArrayList<Integer>> pixels;
     int picWidth , picHeight;
@@ -70,7 +71,7 @@ public class VectorQuantization implements Algorithm {
         }
         return vectors;
     }
-    public ArrayList<ArrayList<Integer>> calculateAvg(ArrayList<ArrayList<ArrayList<Integer>>>vectors){
+    public ArrayList<ArrayList<Integer>> calculateAvg(ArrayList<ArrayList<ArrayList<Integer>>> vectors){
         ArrayList<ArrayList<Integer>> avg = new ArrayList<>();
         for(int i = 0 ; i < vectors.get(0).size() ; i++){
             ArrayList<Integer> temp = new ArrayList<>();
@@ -94,105 +95,66 @@ public class VectorQuantization implements Algorithm {
         }
         return error;
     }
-    public ArrayList<ArrayList<ArrayList<Integer>>> optimize(ArrayList<ArrayList<ArrayList<Integer>>> codebook, ArrayList<ArrayList<ArrayList<Integer>>>vectors ,int[] groups){
-            boolean flag = true;
-            ArrayList<ArrayList<ArrayList<ArrayList<Integer>>>> newGroups = new ArrayList<>();
-            while(flag){
-                flag = false;
-                for(int i = 0 ; i<vectors.size();i++){
-                    double  minError = Double.MAX_VALUE;
-                    int min_index = 0;
-                    for(int j = 0 ; j<codebook.size();j++){
-                        double error = calculateError(vectors.get(i),codebook.get(j));
-                        if(error < minError){
-                            minError = error;
-                            min_index = j;
-                            vectors.get(i).clear();
-                            vectors.get(i).addAll(codebook.get(j));
-                        }
-                    }
-                    if(groups[i] != min_index){
-                        flag = true;
-                        groups[i] = min_index; // update the group of the vector
-                        newGroups.get(min_index).add(vectors.get(i)); // add the vector to the new codebook
-                    }
-                }
-                // calculate the average of each group
-                for(int i = 0 ; i<newGroups.size();i++){
-                    ArrayList<ArrayList<Integer>> avg = calculateAvg(newGroups.get(i));
-                    codebook.set(i , avg);
-                }
-            }
-            return codebook;
-    }
     public ArrayList<ArrayList<ArrayList<Integer>>> getCodesBookHelper(ArrayList<ArrayList<ArrayList<Integer>>> vectors,ArrayList<ArrayList<ArrayList<Integer>>> codebook){
         if(codebook.size() == codebookSize){
-            int[] groups = new int[vectors.size()];
-            for(int i = 0 ; i<vectors.size();i++){
-                double  minError = Double.MAX_VALUE;
-                int min_index = 0;
-                for(int j = 0 ; j<codebook.size();j++){
-                    double error = calculateError(vectors.get(i),codebook.get(j));
-                    if(error < minError){
-                        minError = error;
-                        min_index = j;
-                        vectors.get(i).clear();
-                        vectors.get(i).addAll(codebook.get(j));
-                    }
-                }
-                groups[i] = min_index;
-            }
-            codebook = optimize(codebook , vectors , groups);
             return codebook;
         }
         ArrayList<ArrayList<ArrayList<Integer>>> newCodebook = new ArrayList<>();
         for(int i = 0 ; i < codebook.size() ; i++){
-            ArrayList<ArrayList<Integer>> code1,code2; // code1 is the codebook vector and code2 is the codebook vector + 1
-            code1 = new ArrayList<>(); // initialize the code1 and code2
-            code2 = new ArrayList<>(); // initialize the code1 and code2
-            for(int x = 0 ; x < vectorHeight ; x++){ // loop on the vector height
-                ArrayList<Integer> temp1, temp2; // temp1 is the codebook vector and temp2 is the codebook vector + 1
-                temp1 = new ArrayList<>(); // initialize the temp1 and temp2
-                temp2 = new ArrayList<>(); // initialize the temp1 and temp2
-                for(int y = 0 ; y < vectorWidth ; y++){ // loop on the vector width
-                    temp1.add((int)Math.floor(codebook.get(i).get(x).get(y))); // add the floor of the codebook vector to the temp1
-                    temp2.add((int)Math.floor(codebook.get(i).get(x).get(y))+1); // add the floor of the codebook vector + 1 to the temp2
-                } // end of the loop on the vector width
-                code1.add(temp1); // add the temp1 to the code1
-                code2.add(temp2); // add the temp2 to the code2
+            ArrayList<ArrayList<Integer>> code1,code2;
+            code1 = new ArrayList<>();
+            code2 = new ArrayList<>();
+            for(int x = 0 ; x < vectorHeight ; x++){
+                ArrayList<Integer> temp1, temp2;
+                temp1 = new ArrayList<>();
+                temp2 = new ArrayList<>();
+                for(int y = 0 ; y < vectorWidth ; y++){
+                    temp1.add((int)Math.floor(codebook.get(i).get(x).get(y)));
+                    temp2.add((int)Math.floor(codebook.get(i).get(x).get(y))+1);
+
+                }
+                code1.add(temp1);
+                code2.add(temp2);
             }
-            newCodebook.add(code1); // add the code1 to the new codebook
-            newCodebook.add(code2);  // add the code2 to the new codebook
+            newCodebook.add(code1);
+            newCodebook.add(code2);
         }
-        // now splited the codebook into two codebooks
-        ArrayList<ArrayList<ArrayList<ArrayList<Integer>>>> newGroups = new ArrayList<>(); // initialize the groups array, new Groups is an array of groups
-        for(int i = 0 ; i < newCodebook.size() ; i++){ // initialize the groups array
-            ArrayList<ArrayList<ArrayList<Integer>>> group = new ArrayList<>(); // initialize the group
-            newGroups.add(group); // add the group to the groups array
+        ArrayList<ArrayList<ArrayList<ArrayList<Integer>>>> newGroups = new ArrayList<>();
+        for(int i = 0 ; i < newCodebook.size() ; i++){
+            ArrayList<ArrayList<ArrayList<Integer>>> group = new ArrayList<>();
+            newGroups.add(group);
         }
-        int[] groupIndex = new int[vectors.size()]; // an array to store the index of the group of each vector
-            for(int i = 0 ; i < newCodebook.size() ; i++){ // initialize the groups array
-                ArrayList<ArrayList<ArrayList<Integer>>> group = new ArrayList<>(); // initialize the group
-                newGroups.set(i, group); // add the group to the groups array
+        int[] groupIndex = new int[vectors.size()];
+//        boolean flag = true;
+//        while (flag){
+            for(int i = 0 ; i < newCodebook.size() ; i++){
+                ArrayList<ArrayList<ArrayList<Integer>>> group = new ArrayList<>();
+                newGroups.set(i, group);
             }
-            for(int i = 0 ; i < vectors.size() ; i++){ // loop over all the vectors and assign each one to a group
-                double minError = Integer.MAX_VALUE; // initialize the minimum error
-                int minIndex = 0; // initialize the index of the group with the minimum error
-                for(int j = 0 ; j < newCodebook.size() ; j++){ // loop over all the groups
-                    double error = calculateError(vectors.get(i) , newCodebook.get(j)); // calculate the error between the vector and the group
-                    if(error < minError){ // if the error is less than the minimum error
-                        minError = error; // update the minimum error
-                        minIndex = j; // update the index of the group with the minimum error
+//            flag = false;
+            for(int i = 0 ; i < vectors.size() ; i++){
+                double minError = Integer.MAX_VALUE;
+                int minIndex = 0;
+                for(int j = 0 ; j < newCodebook.size() ; j++){
+                    double error = calculateError(vectors.get(i) , newCodebook.get(j));
+                    if(error < minError){
+                        minError = error;
+                        minIndex = j;
 
                     }
                 }
+//                if(groupIndex[i] != minIndex){
+//                    flag = true;
+//                }
                 groupIndex[i] = minIndex;
                 newGroups.get(minIndex).add(vectors.get(i));
             }
+
             for(int i = 0; i < newCodebook.size() ; i++){
                 if(!newGroups.get(i).isEmpty())
                     newCodebook.set(i,calculateAvg(newGroups.get(i)));
             }
+//        }
 
         return getCodesBookHelper(vectors,newCodebook);
     }
@@ -253,62 +215,105 @@ public class VectorQuantization implements Algorithm {
             }
             compressed.add(minIndex); // add the codebook vector to compressed list
         }
+        String data = "";
         // write the codebook to file
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputPath));
-        bufferedWriter.write(codebook.size() + "\n");
-        bufferedWriter.write(vectorHeight + "\n");
-        bufferedWriter.write(vectorWidth + "\n");
-        bufferedWriter.write(compressed.size() + "\n");
-        bufferedWriter.write(pixels.length + "\n");
-        bufferedWriter.write(pixels[0].length + "\n");
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputPath));
+//        bufferedWriter.write(codebook.size() + "\n");
+//        bufferedWriter.write(vectorHeight + "\n");
+//        bufferedWriter.write(vectorWidth + "\n");
+//        bufferedWriter.write(compressed.size() + "\n");
+//        bufferedWriter.write(pixels.length + "\n");
+//        bufferedWriter.write(pixels[0].length + "\n");
+
+        data += shortToString((short)codebook.size());
+        data += shortToString((short)vectorHeight);
+        data += shortToString((short)vectorWidth);
+        data += shortToString((short)compressed.size());
+        data += shortToString((short)pixels.length);
+        data += shortToString((short)pixels[0].length);
+
         for(int i = 0 ; i < codebook.size() ; i++){
             for(int j = 0 ; j < codebook.get(0).size() ; j++){
                 for(int k = 0 ; k < codebook.get(0).get(0).size() ; k++){
-                    bufferedWriter.write(codebook.get(i).get(j).get(k) + " ");
+//                    bufferedWriter.write(codebook.get(i).get(j).get(k) + " ");
+                    data += byteToString(codebook.get(i).get(j).get(k).byteValue());
                 }
-                bufferedWriter.write("\n");
+//                bufferedWriter.write("\n");
             }
         }
+        int bit_size =(int)(Math.log10(codebook.size()) / Math.log10(2));
+
         for(int i = 0 ; i < compressed.size() ; i++){
-            bufferedWriter.write(compressed.get(i) + " ");
+            for(int j = 0 ; j < bit_size ; j++){
+                if((compressed.get(i) & (1 << (bit_size - j - 1))) != 0){
+                    data += '1';
+                }
+                else{
+                    data += '0';
+                }
+            }
+//            bufferedWriter.write("\n");
         }
-        bufferedWriter.close();
+        char ign =(char)(8 - (data.length() % 8)); // the number of bits to be ignored , as the encoded string may not be a multiple of 8
+        String curr = ""; // to store the encoded string
+        curr += ign; // add the number of bits to be ignored to the beginning of the string, so that it can be retrieved later
+        for(int i = 0 ; i < data.length() ; i+=8){ // iterate over the encoded string
+            curr += (char) stringToByte(data.substring(i , Math.min(i+8 , data.length()))); // add the byte to the string
+        }
+        Files.write(Paths.get(outputPath) , curr.getBytes());
+//        bufferedWriter.close();
     }
 
     @Override
     public void decompress(String inputPath, String outputPath) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(inputPath));
+        String content = Files.readString(Path.of(inputPath));
+        String data = "";
+        for(int i = 0 ; i < content.length() ; i++){
+            data += byteToString((byte)content.charAt(i));
+        }
+
+        int ign = (int)stringToByte(data.substring(0 , 8)); // get the number of bits to be ignored
         // first line
-        int codebookSize = Integer.parseInt(bufferedReader.readLine());
-        // second line
-        vectorHeight = Integer.parseInt(bufferedReader.readLine());
-        // third line
-        vectorWidth = Integer.parseInt(bufferedReader.readLine());
-        // fourth line
-        int compressedSize = Integer.parseInt(bufferedReader.readLine());
-        int height = Integer.parseInt(bufferedReader.readLine());
-        int width = Integer.parseInt(bufferedReader.readLine());
+        int codebookSize = stringToShort(data.substring(8 , 24));
+        int vectorHeight = stringToShort(data.substring(24 , 40));
+        int vectorWidth = stringToShort(data.substring(40 , 56));
+        int compressedSize = stringToShort(data.substring(56 , 72));
+        int height = stringToShort(data.substring(72 , 88));
+        int width = stringToShort(data.substring(88 , 104));
+        int start = 104;
         ArrayList<ArrayList<ArrayList<Integer>>> codebook = new ArrayList<>();
         for(int i = 0 ; i < codebookSize ; i++){
             ArrayList<ArrayList<Integer>> temp = new ArrayList<>();
             for(int j = 0 ; j < vectorHeight ; j++){
                 ArrayList<Integer> temp2 = new ArrayList<>();
-                String [] line = bufferedReader.readLine().split(" ");
                 for(int k = 0 ; k < vectorWidth ; k++){
-                    temp2.add(Integer.parseInt(line[k]));
+                    temp2.add((int) stringToByte(data.substring(start , start + 8) )&0xff);
+                    start += 8;
                 }
                 temp.add(temp2);
             }
             codebook.add(temp);
         }
-        ArrayList<ArrayList<ArrayList<Integer>>> decompressed = new ArrayList<>();
-        String [] line = bufferedReader.readLine().split(" ");
-        for(int i = 0 ; i < line.length ; i++){
-            ArrayList<ArrayList<Integer>> temp = codebook.get(Integer.parseInt(line[i]));
-            decompressed.add(temp);
+
+        int bit_size =(int)(Math.log10(codebook.size()) / Math.log10(2));
+        ArrayList<Integer> compressed = new ArrayList<>();
+        for(int i = 0 ; i < compressedSize ; i++){
+            int num = 0;
+            for(int j = 0 ; j < bit_size ; j++){
+                if(data.charAt(start) == '1'){
+                    num |= (1 << (bit_size - j - 1));
+                }
+                start++;
+            }
+
+            compressed.add(num);
         }
 
-        bufferedReader.close();
+        ArrayList<ArrayList<ArrayList<Integer>>> decompressed = new ArrayList<>();
+        for(int i = 0 ; i < compressed.size() ; i++){
+            decompressed.add(codebook.get(compressed.get(i)));
+        }
+
         ArrayList<ArrayList<Integer>> pixels = new ArrayList<>();
         int width_vector = width / vectorWidth;
         int height_vector = height / vectorHeight;
@@ -319,6 +324,7 @@ public class VectorQuantization implements Algorithm {
             }
             pixels.add(temp);
         }
+
         imageHandler.writeImage(pixels , outputPath);
 
     }
