89f1d2fc7162d55ecc3110bcc22fe4e21ccec0f7
Kareem sakkary <kareemsakkary101@gmail.com>
2023-12-19 00:26:53 +0200

    Add Binary

--- src/PredictiveCompression.java
+++ src/PredictiveCompression.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
@@ -8,6 +9,50 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class PredictiveCompression implements Algorithm{
+
+    public static String byteToString(byte num){ // a method to convert a byte to a string of 8 bits
+        String ret = ""; // the string to be returned
+        for(int i = 7 ; i >= 0 ; i--){ // iterate over the 8 bits of the byte
+            if((num & (1 << i)) != 0){ // if the bit is 1
+                ret += '1'; // add 1 to the string
+            }
+            else{
+                ret += '0'; // add 0 to the string
+            }
+        }
+        return ret; // return the string
+    }
+    public static byte stringToByte(String s){ // a method to convert a string of 8 bits to a byte
+        byte ret = 0; // the byte to be returned
+        for(int i = 0 ; i < s.length() ; i++){ // iterate over the 8 bits of the string
+            if(s.charAt(i) == '1'){ // if the bit is 1
+                ret |= (1 << (7-i)); // set the bit at the same position in the byte to 1
+            }
+        }
+        return ret; // return the byte
+    }
+    public static String shortToString(short num){ // a method to convert a short to a string of 16 bits (2 bytes)
+        String ret = "";
+        for(int i = 15 ; i >= 0 ; i--){
+            if((num & (1 << i)) != 0){
+                ret += '1';
+            }
+            else{
+                ret += '0';
+            }
+        }
+        return ret;
+    }
+    public static short stringToShort(String s){ // a method to convert a string of 16 bits (2 bytes) to a short
+        short ret = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == '1'){
+                ret |= (short) (1 << (15-i));
+            }
+        }
+        return ret;
+    }
+
     @Override
     public String getAlgorithmName() {
         return "2D Predictive Compression";
@@ -21,33 +66,36 @@ public class PredictiveCompression implements Algorithm{
 
     @Override
     public void compress(String inputPath, String outputPath, HashMap<String, Integer> required) throws IOException {
-        int start = -256;
-        int end = 255;
-        int step = required.get("step"); // get the step value from the user
+        short start = -256;
+        short end = 255;
+        short step = required.get("step").shortValue(); // get the step value from the user
         int[][] originalImage = ImageHandler2d.readImage(inputPath); // read the image
-        int height = originalImage.length;
-        int width = originalImage[0].length;
+        short height = (short) originalImage.length;
+        short width = (short) originalImage[0].length;
         HashMap<Integer , Double> deQuantizedValues = getDeQuantizedValues(start , end , step); // get the dequantized values
         int[][] quantizedDifference = predict(originalImage , deQuantizedValues, start , end , step); // get the quantized difference
         String content = "";
-        content+= start + " " + end + " " + step + "\n"; // write the start , end and step
-        content+= height + " " + width + "\n"; // write the height and width
+
+        content+= shortToString(start) +  shortToString(end) +  shortToString(step) ; // write the start , end and step
+        content+= shortToString(height) +  shortToString(width); // write the height and width
         for(int i = 0;i<height;i++){
-            content += originalImage[i][0] + " "; // write the first column
+            content += shortToString((short) originalImage[i][0]) ; // write the first column
         }
-        content += "\n"; // write a new line
         for(int i = 0;i<width;i++){
-            content += originalImage[0][i] + " "; // write the first row
+            content +=  shortToString((short)originalImage[0][i]); // write the first row
         }
         // store the quantized difference
-        content += "\n"; // write a new line
         for(int i = 0;i<height;i++){
             for(int j = 0;j<width;j++){
-                content += quantizedDifference[i][j] + " "; // write the quantized difference
+                content += shortToString((short)quantizedDifference[i][j]); // write the quantized difference
             }
-            content += "\n"; // write a new line
         }
-        Files.write(Paths.get(outputPath) , content.getBytes());
+        String data = "";
+        for(int i = 0 ; i < content.length() ; i+=8){
+            String now = content.substring(i , Math.min(i+8 , content.length()));
+            data += (char)stringToByte(now);
+        }
+        Files.write(Paths.get(outputPath) , data.getBytes());
     }
 
     public int getPredictedValue(int a , int b  , int c){
@@ -111,36 +159,39 @@ public class PredictiveCompression implements Algorithm{
     @Override
     public void decompress(String inputPath, String outputPath, HashMap<String, Integer> required) throws IOException {
        // read from the file
-       File file = new File(inputPath);
        // first line is the start
         // read first line
-        Scanner sc = new Scanner(file);
-       String line1 = sc.nextLine();
-       String[] line1Split = line1.split(" ");
-        int start = Integer.parseInt(line1Split[0]); // get the start
-        int end = Integer.parseInt(line1Split[1]); // get the end
-        int step = Integer.parseInt(line1Split[2]); // get the step
-        String line2 = sc.nextLine();
-        String[] line2Split = line2.split(" ");
-        int height = Integer.parseInt(line2Split[0]); // get the width
-        int width = Integer.parseInt(line2Split[1]); // get the height
-        String firstColumn = sc.nextLine();
-        String[] firstColumnSplit = firstColumn.split(" ");
-        String firstRow = sc.nextLine();
-        String[] firstRowSplit = firstRow.split(" ");
+        String data = Files.readString(Path.of(inputPath));
+        String content = "";
+        for(int i = 0 ; i < data.length() ; i++){
+            content += byteToString((byte)data.charAt(i));
+        }
+        int index = 0;
+        int start = stringToShort(content.substring(index,index+16));// get the start
+        index+=16;
+        int end = stringToShort(content.substring(index,index+16)); // get the end
+        index+=16;
+        int step = stringToShort(content.substring(index,index+16)); // get the step
+        index+=16;
+        int height = stringToShort(content.substring(index,index+16)); // get the width
+        index+=16;
+        int width = stringToShort(content.substring(index,index+16)); // get the height
+        index+=16;
+
         int[][] decodedImage = new int[height][width]; // create a new image to store the decoded image
         for(int i = 0;i<height;i++){
-            decodedImage[i][0] = Integer.parseInt(firstColumnSplit[i]); // take the first column
+            decodedImage[i][0] = stringToShort(content.substring(index,index+16)); // take the first column
+            index+=16;
         }
         for(int i = 0;i<width;i++){
-            decodedImage[0][i] = Integer.parseInt(firstRowSplit[i]); // take the first row
+            decodedImage[0][i] =  stringToShort(content.substring(index,index+16)); // take the first row
+            index+=16;
         }
         int[][]quantizedDifference = new int[height][width]; // create a new image to store the quantized difference
         for(int i = 0;i<height;i++){
-            String line = sc.nextLine();
-            String[] lineSplit = line.split(" ");
             for(int j = 0;j<width;j++){
-                quantizedDifference[i][j] = Integer.parseInt(lineSplit[j]); // take the quantized difference
+                quantizedDifference[i][j] =  stringToShort(content.substring(index,index+16)); // take the quantized difference
+                index+=16;
             }
         }
         HashMap<Integer , Double> deQuantized = getDeQuantizedValues(start , end , step); // get the dequantized values
